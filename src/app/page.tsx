f29bb2efeff68b2675edaaefc5de8d95827c90d9
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2024-07-21 13:16:43 +0700

    setup1

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="container mx-auto my-3">
+      <h1>MiniCRUD with NextJS + MongoDB</h1>
+      <hr className="my-3"/>
+      <div></div>
+    </main>
+  );
+}
